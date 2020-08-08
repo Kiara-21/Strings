b652de596560b78787ccac0f7dfe40677c9d72bf
Kiara-21 <bodyukiara@gmail.com>
2020-08-08 13:18:28 +0300

    c0

---
+++ Lab_6_C_style.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	char s[80];
+	cout << "Load text: ";
+	cin.getline(s, 80);
+	int a, i;
+	a = 0;
+
+	for (i = 0; i < strlen(s) - 1; i++)
+		if ((s[i] == ' ') && (s[i + 1] != ' '))
+			a = a + 1;
+	if (s[0] != ' ')
+		a = a + 1;
+	
+	cout << "Number of words: " << a << endl;
+	
+	char* p, * q;
+	
+	if (s == NULL)
+		return EOF;
+	
+	for (p = q = s; (*p = *q) != '\0'; p++, q++)
+		if (*p == ' ')
+			while (*(q + 1) == ' ')
+				q++;
+	
+	cout << "\n" << s << endl;
+	
+	char* t, * w = NULL, d[] = " .,!:\t\n";
+	int m = -1;
+	
+	t = strtok(s, d);
+	
+	while (t != NULL) {
+
+		int v = 0;
+		char* o = t;
+		while (*o != '\0') if (strchr("Aa,Ee,Ii,Oo,Uu", *o++) != NULL)
+			++v;
+		if (v > m) {
+			w = t;
+			m = v;
+		}
+		t = strtok(NULL, d);
+	}
+
+	if (w != NULL) printf("Biggest of vowel -: %s\n", w);
+
+	return 0;
+}
---
+++ lab_6_C++_style.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main()
+{
+	string text; //Строка
+	string word, temp, big_glass;
+	stringstream tx;
+	string glass = "AaEeIiOoUuYy"; //Гласные буквы
+	size_t len = 0;
+	
+	int c = 0; //Переменная для хранения количества слов
+	
+	cout << "Load text" << endl;
+	
+	getline(cin, text); //Ввод строки
+	text = " " + text;
+	for (int i = 0; i < text.length(); i++) {
+		if (text[i] == ' ') {
+			c++;
+			if (text[i + 1] == ' ')
+				c--;
+		}
+	} //Подсчет количества слов в строке
+	
+	text.erase(0, 1);
+	cout << "Number of words: " << c << endl;
+	tx << text;
+	
+	while (tx >> word) {
+		temp = word;
+		for (size_t i = 0; i < temp.length(); i++)
+			if (glass.find(temp[i]) != std::string::npos)
+
+				word += temp[i];
+		if (word.length() - temp.length() > len) {
+			len = word.length() - temp.length();
+			big_glass = temp;
+		}
+	} //Поиск числа с наибольшим количеством гласных букв
+	
+	cout << "Biggest of vowels - " << big_glass << endl;
+	
+	int k, m;
+	m = 0;
+	
+	for (int i = 0; i < text.length(); i++)
+	{
+	EX_SPACE:
+		if (text[i] == ' ' && text[i + 1] == ' ')
+		{
+			k = i + 1;
+			text.erase(k, 2);
+			if (text[i + 2] == ' ')
+				goto EX_SPACE;
+		}
+	} //Удаление лишних пробелов из текста
+
+	cout << "Without excess spaces: " << endl;
+	cout << text;
+	return 0;
+}
